2379387724726cd3b9470d821fc890862cd517c4
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-11 19:37:55 -0300

    Se implementa la pagina del login

---
+++ js/login.js
@@ -0,0 +1,9 @@
+let boton = document.getElementById("botonLogin")
+
+
+boton.addEventListener("click",function(){
+    let mail = document.getElementById("emailInicio").value;
+    let contra = document.getElementById("contraInicio").value;
+    if(mail!='' && contra!='')
+        window.location.href = "index.html"
+})
