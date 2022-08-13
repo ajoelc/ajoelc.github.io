2a46536ac5acbc31d0a83ab5993d60c0d5b31aec
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-13 14:22:26 -0300

    Agrega onsubmit

--- js/login.js
+++ js/login.js
@@ -1,9 +1,4 @@
-let boton = document.getElementById("botonLogin")
-
-
-boton.addEventListener("click",function(){
-    let mail = document.getElementById("emailInicio").value;
-    let contra = document.getElementById("contraInicio").value;
-    if(mail!='' && contra!='')
-        window.location.href = "inicio.html"
-})
+function redirect(){
+    window.location.href = "inicio.html";
+    return false;
+}
