d95645ad260f0d1e80f473fe6073bbfb17e840c1
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-19 10:48:31 -0300

    Se quita onsubmit

--- js/login.js
+++ js/login.js
@@ -2,3 +2,7 @@ function redirect(){
     window.location.href = "inicio.html";
     return false;
 }
+
+let formLogin = document.getElementById("formLogin");
+
+formLogin.addEventListener("submit",redirect());
