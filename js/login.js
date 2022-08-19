eabcd888737513d706434e02b08883be1b2269a5
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-19 10:58:11 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,9 @@
-function redirect(){
+function redirect(evento){
+    evento.preventDefault();
     window.location.href = "inicio.html";
     return false;
 }
 
 let formLogin = document.getElementById("formLogin");
 
-formLogin.addEventListener("submit",redirect());
+formLogin.addEventListener("submit",redirect(evento));
