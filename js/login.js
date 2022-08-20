3b553eea0cca773ef606f59f67cc62a2eab36536
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-20 18:54:57 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,8 +5,18 @@ function redirect(){
     return false;
 }
 
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
 function redirectGoogle(response){
-    const responsePayload = decodeJwtResponse(response.credential);
+    const responsePayload = parseJwt(response.credential);
     console.log('Full Name: ' + responsePayload.name);
     console.log("Email: " + responsePayload.email);
     window.location.href = "inicio.html";
