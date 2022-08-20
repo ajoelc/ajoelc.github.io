e95ac4734699f2568c912e54dfeeda82427c8568
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-20 19:29:55 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 function redirect(){
     window.location.href = "inicio.html";
+    localStorage.clear();
     let mail = document.getElementById("emailInicio").value;
     localStorage.setItem('mail', mail);
     return false;
