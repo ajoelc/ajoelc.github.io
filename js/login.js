21fb1368c1a4ebfb59444a0cb517786c36481bbe
Joel Cabrera <0ajoelc2@gmail.com>
2022-08-20 18:28:33 -0300

    Se cambiar callback y funcion en logis.js

--- js/login.js
+++ js/login.js
@@ -4,3 +4,14 @@ function redirect(){
     localStorage.setItem('mail', mail);
     return false;
 }
+
+function redirectGoogle(response){
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+}
